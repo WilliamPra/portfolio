4c12ea7c885771eafb8c571e843730497860ecb0
Valentin Crochemore <valentin.crochemore@gmail.com>
2017-04-01 19:26:19 +0200

    Add bootstrap and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,50 @@
 'use strict';
 
 var gulp = require('gulp');
+
+// Modules for dev env
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+
+// Modules for prod env
 var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
-var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
+
+// Modules for various purpose
+var runSequence = require('run-sequence');
+var gulpIf = require('gulp-if');
 var cache = require('gulp-cache');
 var del = require('del');
-var runSequence = require('run-sequence');
 
-// default task
+// Project config objects
+var devBaseDir = 'app';
+var dev = {
+    dir: {
+        sass: devBaseDir + '/scss',
+        css: devBaseDir + '/css',
+        js: devBaseDir + '/js',
+        images: devBaseDir + '/images',
+        fonts: devBaseDir + '/fonts'
+    }
+};
+
+var prodBaseDir = 'dist';
+var prod = {
+    dir: {
+        images: prodBaseDir + '/images',
+        fonts: prodBaseDir + '/fonts'
+    }
+};
+
+//--------------------------------------------------------|
+// Gulp tasks for development env
+//--------------------------------------------------------|
+
+// Default task
+// Run Sass, Browserify and Watch tasks
 gulp.task('default', function (callback) {
    runSequence(['sass', 'browserSync', 'watch'],
         callback
@@ -20,66 +52,94 @@ gulp.task('default', function (callback) {
 });
 
 // Watchers
+// Watch HTML files then reload web server
+// Watch Sass files then run Sass task
+// Watch JavaScript files then reload web server
 gulp.task('watch', ['browserSync', 'sass'], function () {
-    gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/scss/**/*.scss', ['sass']);
-    gulp.watch('app/js/**/*.js', browserSync.reload);
+    gulp.watch(devBaseDir + '/*.html', browserSync.reload);
+    gulp.watch(dev.dir.sass + '/**/*.scss', ['sass']);
+    gulp.watch(dev.dir.js + '/**/*.js', browserSync.reload);
 });
 
-// Browserify
+// Browserify config
+// Spin up a web server to do live-reloading
 gulp.task('browserSync', function () {
    browserSync.init({
        server: {
-           baseDir: 'app'
+           baseDir: devBaseDir
        }
    })
 });
 
-// Sass to CSS task
+// Sass task
+// Transform Sass to CSS
+// Reload web server
 gulp.task('sass', function () {
-   return gulp.src('app/scss/**/*.scss')
-       .pipe(sass())
-       .pipe(gulp.dest('app/css'))
+   return gulp.src(dev.dir.sass + '/**/*.scss')
+       .pipe(sass().on('error', sass.logError))
+       .pipe(autoprefixer({
+           cascade: false
+       }))
+       .pipe(gulp.dest(dev.dir.css))
        .pipe(browserSync.reload({
            stream: true
        }))
 });
 
-// Useref task to concatenate js and css files
-gulp.task('useref', function () {
-   return gulp.src('app/*.html')
-       .pipe(useref())
-       .pipe(gulpIf('*.js', uglify()))
-       .pipe(gulpIf('*.css', cssnano()))
-       .pipe(gulp.dest('dist'))
-});
+//--------------------------------------------------------|
+// Gulp tasks for production env
+//--------------------------------------------------------|
 
-// Images task to minify images
-gulp.task('images', function () {
-   return qulp.src('app/images/**/*.+(png|jpg|gif|svg')
-       .pipe(cache(imagemin()))
-       .pipe(gulp.dest('dist/images'))
-});
-
-// Fonts task to copy fonts from app to dist
-gulp.task('fonts', function () {
-   return gulp.src('app/fonts/**/*')
-       .pipe(gulp.dest('dist/fonts'))
-});
-
-// Task to clean dist folder
-gulp.task('clea:dist', function () {
-   return del.sync('dist')
-});
-
-// Task to clear local cache
-gulp.task('cache:clear', function (callback) {
-    return cache.clearAll(callback)
-});
-
+// Build task
+// Run this task when site is ready for production
+// Run Clean dist task first
+// Then Sass, Useref, Images and fonts tasks
 gulp.task('build', function (callback) {
     runSequence('clean:dist',
         ['sass', 'useref', 'images', 'fonts'],
         callback
     )
+});
+
+// Useref task
+// Concatenate JS and CSS files called in HTML files
+// Minify JS and CSS files
+gulp.task('useref', function () {
+    return gulp.src(devBaseDir + '/*.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest(prodBaseDir))
+});
+
+// Images task
+// Minify images for production
+// Accept PNG, JPG, GIF and SVG
+gulp.task('images', function () {
+    return qulp.src(dev.dir.images + '/**/*.+(png|jpg|gif|svg')
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest(prod.dir.images))
+});
+
+// Fonts task
+// Copy fonts from app to dist
+gulp.task('fonts', function () {
+    return gulp.src(dev.dir.fonts + '/**/*')
+        .pipe(gulp.dest(prod.dir.fonts))
+});
+
+// Clean dist task
+// Delete every files and folder in dist to avoid useless files
+gulp.task('clean:dist', function () {
+    return del.sync(prodBaseDir)
+});
+
+//--------------------------------------------------------|
+// Gulp tasks for other purpose
+//--------------------------------------------------------|
+
+// Cache clear task
+// Task to clear locale cache
+gulp.task('cache:clear', function (callback) {
+    return cache.clearAll(callback)
 });
